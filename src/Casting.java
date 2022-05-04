cafd7775a84eaa3282af3e12017673198aea19da
Rutti <ruttib@gmail.com>
2022-05-04 09:14:59 -0400

    updated

--- src/Casting.java
+++ src/Casting.java
@@ -1,17 +1,15 @@
 class Casting {
   public static void main(String[] args) {
-    // create double type variable
+
     double num = 112.35;
     System.out.println("Double: " + num);
 
-    // convert into int type
     int data = (int)num;
     System.out.println("int: " + data);
 
     String str = "49";
     System.out.println("The string value is: " + str);
 
-    // convert string variable to int
     int number = Integer.parseInt(str);
     System.out.println("The integer value is: " + number);
   }
